49b4424a88a5944a24a6d8e883c3a14955bfdeaa
Volha Shut <Volha_Shut@epam.com>
2017-04-14 13:33:01 +0300

    complited feature parser

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 const gulp = require('gulp');
 const util = require('gulp-util');
-const finder=require('./getFiles').finder;
-const reader=require('./readFeatures').reader;
+const finder=require('./lib/getFiles').finder;
+const reader=require('./lib/readFeatures').reader;
 const path=require('path');
 const fs=require('fs');
 
@@ -10,21 +10,19 @@ gulp.task('read:features', ()=>{
     let dirname=path.normalize(util.env.path);
     let files=finder(dirname);
     reader(files).then((features)=>{
-        fs.writeFileSync('features.json',JSON.stringify(features),'utf8');
-        console.log('done');
-    })
-
-
+        fs.writeFileSync('./output/features.json',JSON.stringify(features),'utf8');
+        console.log('done!');
+    });
 });
 
 gulp.task('post:jira', ()=>{
-    require('./rest_request');
+    require('./lib/rest/rest_request');
 });
 
 gulp.task('create-session', ()=>{
-    require('./rest-client');
+    require('./lib/rest/rest-client');
 });
 
 gulp.task('delete-session', ()=>{
-    require('./rest-delete-session');
+    require('./lib/rest/rest-delete-session');
 });
